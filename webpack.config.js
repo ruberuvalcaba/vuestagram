252ff5acdb6b95fdf0411025d7caaa63f3a3a020
Ruben Flores <ruben_flores@epam.com>
2017-08-03 17:04:42 -0500

    Modifies webpack config - paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-var utils = require('./utils')
+var utils = require('../build/utils.js')
 var webpack = require('webpack')
-var config = require('../config')
+var config = require('./config')
 var merge = require('webpack-merge')
-var baseWebpackConfig = require('./webpack.base.conf')
+var baseWebpackConfig = require('./build/webpack.base.conf.js')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
